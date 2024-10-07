2099ba3f4e590eb624dc848761ad232351c59fa4
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-10-07 12:07:16 +0530

    complete crud operations in note taker

---
+++ src/main/java/lk/ijse/gdse/aad68/notetaker/dto/Impl/NoteDTO.java
@@ -0,0 +1,21 @@
+package lk.ijse.gdse.aad68.notetaker.dto.Impl;
+
+import lk.ijse.gdse.aad68.notetaker.customObj.NoteResponse;
+import lk.ijse.gdse.aad68.notetaker.dto.SuperDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class NoteDTO implements SuperDTO , NoteResponse {
+    private String noteId;
+    private String noteTitle;
+    private String noteDesc;
+    private String priorityLevel;
+    private String createDate;
+    private String userId;
+
+}
